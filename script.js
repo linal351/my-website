010bbfbd2855eca89eaac581b8cf8caa3933fb8f
linal351 <linal2023brother@gmail.com>
2026-06-23 21:14:20 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Smooth scroll for button
-document.querySelector('.btn').addEventListener('click', function(e) {
-  e.preventDefault();
-  document.querySelector('#video').scrollIntoView({
-    behavior: 'smooth'
-  });
-});
